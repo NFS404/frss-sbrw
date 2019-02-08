0d504492d96ff3af94fb3d7adfac756e2fc74174
redbluescreen <redbluescreen@nextdata.eu>
2019-02-08 13:11:41 +0200

    Remove request logging

--- src/main/java/com/soapboxrace/core/api/util/HttpHeaderFilter.java
+++ src/main/java/com/soapboxrace/core/api/util/HttpHeaderFilter.java
@@ -1,7 +1,5 @@
 package com.soapboxrace.core.api.util;
 
-import java.io.IOException;
-
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -11,8 +9,7 @@ import javax.ws.rs.ext.Provider;
 public class HttpHeaderFilter implements ContainerResponseFilter {
 
 	@Override
-	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-		System.out.println("[" + request.getMethod() + "] " + request.getUriInfo().getPath() + " " + request.getUriInfo().getRequestUri().getQuery());
+	public void filter(ContainerRequestContext request, ContainerResponseContext response) {
 		response.getHeaders().add("Connection", "close");
 	}
 }
