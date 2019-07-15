fc643443f90248f614bc6d228464756cbdd9cca9
redbluescreen <redbluescreen@nextdata.eu>
2019-07-15 16:35:40 +0300

    Fix brands of Shelby cars

--- src/main/java/com/soapboxrace/core/bo/CarBO.java
+++ src/main/java/com/soapboxrace/core/bo/CarBO.java
@@ -49,8 +49,8 @@ public class CarBO
         carBrandMap.put("CHEVELLE", "CHEVROLET");
         carBrandMap.put("CLIO", "RENAULTSPORT");
         carBrandMap.put("COBALT", "CHEVROLET");
-        carBrandMap.put("COBRA", "CHEVROLET");
-        carBrandMap.put("COBRADAYTONA", "CHEVROLET");
+        carBrandMap.put("COBRA", "SHELBY");
+        carBrandMap.put("COBRADAYTONA", "SHELBY");
         carBrandMap.put("CONTINENTAL", "BENTLEY");
         carBrandMap.put("COROLLA", "TOYOTA");
         carBrandMap.put("CORVETTE", "CHEVROLET");
