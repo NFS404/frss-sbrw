3a674229decb893a1f77655e04460e1fee4046f0
redbluescreen <redbluescreen@nextdata.eu>
2019-08-31 13:13:01 +0300

    Get chat rooms only when needed in ChatAnnouncementsBO

--- src/main/java/com/soapboxrace/core/bo/ChatAnnouncementsBO.java
+++ src/main/java/com/soapboxrace/core/bo/ChatAnnouncementsBO.java
@@ -32,19 +32,23 @@ public class ChatAnnouncementsBO
     {
         ticks += 5;
 
+        List<MUCRoomEntity> allRooms = null;
+
         for (ChatAnnouncementEntity announcementEntity : chatAnnouncementDAO.findAll())
         {
             if (announcementEntity.getAnnouncementInterval() % 5 != 0) continue;
 
-            List<MUCRoomEntity> channels = restApiCli.getAllRooms()
-                    .stream()
-                    .filter(r -> r.getRoomName().startsWith(announcementEntity.getChannelMask().replace("*", "")))
-                    .collect(Collectors.toList());
-
-            String message = XmppChat.createSystemMessage(announcementEntity.getAnnouncementMessage());
-
             if (ticks % announcementEntity.getAnnouncementInterval() == 0)
             {
+                if (allRooms == null) {
+                    allRooms = restApiCli.getAllRooms();
+                }
+                List<MUCRoomEntity> channels = allRooms.stream()
+                        .filter(r -> r.getRoomName().startsWith(announcementEntity.getChannelMask().replace("*", "")))
+                        .collect(Collectors.toList());
+
+                String message = XmppChat.createSystemMessage(announcementEntity.getAnnouncementMessage());
+
                 for (MUCRoomEntity channel : channels)
                 {
                     List<Long> members = restApiCli.getAllOccupantsInRoom(channel.getRoomName());
